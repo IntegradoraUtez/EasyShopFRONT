4fde47de722c5d9c85c98416ecce70b45f239386
Julio4647 <20213tn079@utez.edu.mx>
2024-07-25 22:50:22 -0600

    commit Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,37 @@
 //Julio
 import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap';
+import { Link } from "react-router-dom";
+
 
 function Footer() {
   return (
-    <div>
-      
-    </div>
+    <footer className="bg-light text-dark py-3 footerStyle">
+            <Container>
+                <Row className="align-items-center">
+                    <Col xs={12} md={6}>
+                        <Link className="navbar-brand" to="/">
+                            <img src="" alt="EasyShop" className="imagenLogoFooter" />
+                        </Link>
+                    </Col>
+                    <Col xs={12} md={6} className="text-md-end">
+                        <ul className="nav justify-content-center justify-content-md-end">
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/">Inicio</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/category">Category</Link>
+                            </li>
+                        </ul>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col className="text-center">
+                        <p>&copy; {new Date().getFullYear()} EasyShop. Todos los derechos reservados.</p>
+                    </Col>
+                </Row>
+            </Container>
+        </footer>
   )
 }
 
