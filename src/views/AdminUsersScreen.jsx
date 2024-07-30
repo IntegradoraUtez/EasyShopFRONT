b28f1ba7b682583b81053bb82a0b9b65d821fa6e
AaronGT03 <20213tn061@utez.edu.mx>
2024-07-29 21:23:46 -0600

    commit AdminUsers

---
+++ src/views/AdminUsersScreen.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ProfileComponents from './../components/profile/ProfileComponent'
+
+function AdminUsersScreen() {
+  return (
+      <ProfileComponents/>
+  )
+}
+
+export default AdminUsersScreen
