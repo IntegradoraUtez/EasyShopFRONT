406c960ea661b513440e7d5ff8ff911edc8c3d72
AaronGT03 <20213tn061@utez.edu.mx>
2024-08-18 19:46:12 -0600

    Commit GET listUsersAdmin

--- src/components/admin/AdminUsersScreenComponents.jsx
+++ src/components/admin/AdminUsersScreenComponents.jsx
@@ -1,21 +1,47 @@
-import React, {useEffect} from 'react';
+import React, { useEffect, useState } from 'react';
 import './AdminUsersScreenComponents.css'; 
-import { useAuth } from '../../context/AuthContext';
 import { useNavigate } from 'react-router-dom';
+
 function AdminUsersScreenComponents() {
-  const {user} = useAuth();
-  const navigate = useNavigate;
-    
+  const [users, setUsers] = useState([]);
+  const storedUser = localStorage.getItem('user');
+  const user = storedUser ? JSON.parse(storedUser) : null;
+
+  const storedToken = localStorage.getItem('token');
+  const token = storedToken || null;
+
+  const navigate = useNavigate();
+
   useEffect(() => {
-      if (!user || user.user.type !== 'admin') {
-          navigate('/'); 
-      } else {
-          console.log('Tipo:', user.user.type);
-          console.log('Token:', user.token);
-      }
+    if (!user || user.user.type !== 'admin') {
+      navigate('/'); 
+    } else {
+      console.log('Tipo:', user.user.type);
+      console.log('Token:', user.token);
 
-  }, [user, navigate]);
-
+      fetch('https://ewjkx0lte6.execute-api.us-east-1.amazonaws.com/Prod/get_users', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${user.token}`, // Si se requiere token de autorización
+        },
+      })
+        .then(response => {
+          if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+          }
+          return response.json();
+        })
+        .then(data => {
+          if (data && Array.isArray(data.users)) {
+            setUsers(data.users);
+          } else {
+            console.error('Error al obtener los usuarios:', data);
+          }
+        })
+        .catch(error => console.error('Error en la solicitud:', error));
+    }
+  }, [user, navigate, token]);
 
   return (
     <div className="user-table-container">
@@ -32,29 +58,19 @@ function AdminUsersScreenComponents() {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>juanp</td>
-            <td>juanp@example.com</td>
-            <td>Juan Pérez</td>
-            <td>123-456-7890</td>
-            <td>Admin</td>
-            <td>
-              <button className="action-button update">Actualizar</button>
-              <button className="action-button disable">Deshabilitar</button>
-            </td>
-          </tr>
-          <tr>
-            <td>anag</td>
-            <td>anag@example.com</td>
-            <td>Ana García</td>
-            <td>098-765-4321</td>
-            <td>Usuario</td>
-            <td>
-              <button className="action-button update">Actualizar</button>
-              <button className="action-button disable">Deshabilitar</button>
-            </td>
-          </tr>
-          {/* Añadir más filas aquí */}
+          {users.map((user, index) => (
+            <tr key={index}>
+              <td>{user.username}</td>
+              <td>{user.email}</td>
+              <td>{user.name} {user.lastname}</td>
+              <td>{user.phone}</td> {/* Asumiendo que hay un campo de teléfono */}
+              <td>{user.type}</td>
+              <td>
+                <button className="action-button update">Actualizar</button>
+                <button className="action-button disable">Deshabilitar</button>
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
