ca215384431e5ad43350bc5064636c2f83782450
ZurielCK <20213tn085@utez.edu.mx>
2024-08-19 16:40:56 -0600

    homeCarrusel

--- src/components/landing/CarrouselHome.jsx
+++ src/components/landing/CarrouselHome.jsx
@@ -1,47 +1,41 @@
-import React from 'react'
+import React from 'react';
 import { Carousel } from 'react-bootstrap';
-import './styleHomeScreen.css'
+import './styleHomeScreen.css';
 
-function CarrouselHome() {
+function CarrouselHome({ images }) {
     return (
         <>
             <Carousel className="custom-carousel">
-                <Carousel.Item>
-                    <img
-                        className="d-block w-100"
-                        src="https://via.placeholder.com/800x400"
-                        alt="First slide"
-                    />
-                    <Carousel.Caption>
-                        <h3>First slide label</h3>
-                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                        className="d-block w-100"
-                        src="https://via.placeholder.com/800x400"
-                        alt="Second slide"
-                    />
-                    <Carousel.Caption>
-                        <h3>Second slide label</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                        className="d-block w-100"
-                        src="https://via.placeholder.com/800x400"
-                        alt="Third slide"
-                    />
-                    <Carousel.Caption>
-                        <h3>Third slide label</h3>
-                        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
+                {images && images.length > 0 ? (
+                    images.map((image, index) => (
+                        <Carousel.Item key={index}>
+                            <img
+                                className="d-block w-100"
+                                src={image.url}
+                                alt={`Slide ${index + 1}`}
+                            />
+                            <Carousel.Caption>
+                                <h3>{`Slide ${index + 1}`}</h3>
+                                <p>{image.description || 'Descripción de la imagen'}</p>
+                            </Carousel.Caption>
+                        </Carousel.Item>
+                    ))
+                ) : (
+                    <Carousel.Item>
+                        <img
+                            className="d-block w-100"
+                            src="https://via.placeholder.com/800x400"
+                            alt="Placeholder slide"
+                        />
+                        <Carousel.Caption>
+                            <h3>No hay imágenes disponibles</h3>
+                            <p>Por favor, intente más tarde.</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                )}
             </Carousel>
         </>
-    )
+    );
 }
 
-export default CarrouselHome
+export default CarrouselHome;
